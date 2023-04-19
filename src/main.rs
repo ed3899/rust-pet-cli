2d0f2aebd8d76263c4a22b7206ba1daccf6c2c96
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-04-19 18:06:36 +0000

    added crossterm backend

--- src/main.rs
+++ src/main.rs
@@ -96,6 +96,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         loop {
             let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+
+            if event::poll(timeout).expect("poll works") {
+                if let CEvent::Key(key) = event::read().expect("can read events") {
+                    tx.send(Event::Input(key)).expect("can send events");
+                }
+            }
+
+            if last_tick.elapsed() >= tick_rate {
+                if let Ok(_) = tx.send(Event::Tick) {
+                    last_tick = Instant::now();
+                }
+            }
         }
-    })
+    });
+
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+
+    let menu_titles = vec!["Home", "Pets", "Add", "Delete", "Quit"];
+    let mut active_menu_item = MenuItem::Home;
+    let mut pet_list_state = ListState::default();
+    pet_list_state.select(Some(0));
+
+    
 }
